d77e2812978d8f926b771008e1be8aa2c0292bb9
Omkar Date <omkardate29@gmail.com>
2023-11-17 23:44:04 +0530

    chapter 11 complete

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme | Customers',
+  description: 'Acme is a fictional company.',
+};
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,6 @@
 import { Poppins } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
 
 export const poppins = Poppins({
   subsets: ['devanagari'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
-
-export const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: ['400', '700'],
 });
